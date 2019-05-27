56b3b6371ac7af58627faf63c7af21e8d70eee50
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2019-05-27 13:36:20 +0300

    Cleaner trimming

--- Imag/main.cpp
+++ Imag/main.cpp
@@ -15,14 +15,11 @@ const char windowCutStart[] = "Cut";
 const char windowCutEnd[] = "Cut End";
 const char windowRoi[] = "ROI";
 
-/*
-Blurring exsamples from:
-https://docs.opencv.org/2.4.13.2/doc/tutorials/imgproc/gausian_median_blur_bilateral_filter/gausian_median_blur_bilateral_filter.html
-*/
-
-static void detectLines(cv::Mat &scr_gray, cv::Mat &src_display);
-
 static void loadVideo(std::string &vidPath);
+
+static void trimStartCallbeck(int val, void* object);
+static void trimEndCallbeck(int val, void* object);
+static void timeLineDragCallback(int val, void* object);
 
 /* Console input y/n. */
 static bool askYesNo(const char* promt);
@@ -40,6 +37,7 @@ std::string vidType = ".mp4";
 std::string saveFolder = "saved//";
 
 /* How long time a single frame takes. */
+int frameRate = 30;
 double timePerFrameMs = 33.;
 int frameWidth = 120;
 int frameHeight = 120;
@@ -76,66 +74,13 @@ int rotation = 0;
 int main() {
 
 
-	/// Create Windows
-	cv::namedWindow(windowName, 1);
-
-	/// Reduce the noise so we avoid false circle detection
-	//cv::GaussianBlur(srcBW, srcBW, cv::Size(9, 9), 2, 2);
-
-	/*
-	TODO:
-
-	rotate - ok
-	detect framerate - ok
-	detect width, height and center point - ok
-	scratch the beginning and the end times - ok
-	go to first frame and:
-		detect circles - ok
-		run search for the rest of the frames and follow the circle
-		save circle's center points
-	analyze stuff from center points
-
-	*/
-
 	std::string vidPath = vidFolder + vidName + vidType;
 
 	loadVideo(vidPath);
 
+	cv::namedWindow(windowName, 1);
 	showingFrame = timeLine[cutStartFrame];
-	cv::Mat frameGray;
-	cv::cvtColor(showingFrame, frameGray, cv::COLOR_RGB2GRAY);
-
-	int kl = 3;
-	int maxKl = 25;
-
-	/*
-	The last parameter, &src, is fucking important!!
-	Never, ever remove it!
-	Last example of:
-	http://answers.opencv.org/question/91462/trackbar-pass-variable/
-	*/
-	//cv::createTrackbar("Kernel size", windowName, &kl, maxKl, blur, &src);
-	//blur(kl, &frameGray);
-	//cv::waitKey(0);
-
-	int tresh = 1;
-	int maxTresh = 255;
-
-//	std::cout << "Drag the slider until the barbell is clearly visible." << std::endl;
-
-	// Binarization
-	//cv::createTrackbar("Kernel size", windowName, &tresh, maxTresh, binarizeCallback, &frameGray);
-	//binarizeCallback(tresh, &frameGray);
-
-	// Debug
-	binVal = 65;
-
-	//cv::waitKey(0);
-
-	std::cout << "Binarization kernel size set to " << binVal << "." << std::endl;
-
-	//detectFirstCircle();
-
+	
 	centerPoints = std::vector<cv::Point>(cutLength);
 	listPosY = std::vector<float>(cutLength);
 	roi = cv::selectROI(windowName, showingFrame);
@@ -162,9 +107,9 @@ int main() {
 	// Setup the termination criteria, either 10 iteration or move by atleast 1 pt
 	cv::TermCriteria term_crit(cv::TermCriteria::EPS | cv::TermCriteria::COUNT, 10, 1);
 
-	for(int i = 0; i < cutLength; ++i) {
+	for(int i = 0; i <  cutLength; ++i) {
 		cv::Mat hsv, dst;
-		frame = timeLine[i];
+		frame = timeLine[i + cutStartFrame];
 		if(frame.empty())
 			break;
 		cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
@@ -178,14 +123,15 @@ int main() {
 		centerPoints[i] = center;
 		listPosY[i] = center.y;
 		circle(frame, center, 3, circleColor, -1, 8, 0);
-
-		//cv::imshow(windowName, frame);
-		//int keyboard = cv::waitKey(30);
-		//if(keyboard == 'q' || keyboard == 27)
-		//	break;
 	}
 
 	std::cout << "done" << std::endl;
+
+	// Allow user to drag the analyzed video
+	cv::destroyWindow(windowName);
+	cv::namedWindow(windowName, 1);
+	cv::createTrackbar("Frame", windowName, 0, cutLength - 1, timeLineDragCallback);
+	timeLineDragCallback(0, 0);
 
 	std::cout << "Analyzing..." << std::endl;
 
@@ -195,9 +141,107 @@ int main() {
 	s.analyze();
 	s.printCsv();
 
+	std::cout << "done" << std::endl;
+
 	cv::waitKey(0);
 }
 
+/*
+Load a video to the timeLine and ask for video cut.
+*/
+static void loadVideo(std::string &vidPath) {
+
+	cv::VideoCapture cap(vidPath);
+	cv::Mat frame;
+
+	if(!cap.isOpened()) {
+		std::cerr << "Opening video file from " << vidPath << " failed." << std::endl;
+	}
+
+	std::cout << "Loading video... " << std::endl;
+
+	// take the first frame for getting some basic info about the video
+	// The first frame has to be read before 
+	//cap.read(frame);
+
+	frameRate = cap.get(cv::CAP_PROP_FPS);
+	timePerFrameMs = 1.f / (float)frameRate;
+	frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
+	frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+	initialFrameCount = cap.get(cv::CAP_PROP_FRAME_COUNT);
+	cutEndFrame = initialFrameCount;
+	roi.width = frameWidth;
+	roi.height = frameHeight;
+	timeLine = std::vector<cv::Mat>(initialFrameCount);
+
+	
+
+	for(size_t i = 0; i < timeLine.size(); ++i) {
+		cap.read(frame);
+		timeLine[i] = frame.clone();
+		FrameUtils::rotate_90n(timeLine[i], timeLine[i], rotation);
+	}
+	cap.release();
+	std::cout << "done" << std::endl;
+	
+
+	cv::destroyWindow(windowName);
+	std::cout << "Drag the timeline just before the beginning of a squat. Press any key to confirm." << std::endl;
+	cv::namedWindow(windowName, 1);
+	cv::createTrackbar("Trim start", windowName, 0, initialFrameCount - 1, trimStartCallbeck);
+	trimStartCallbeck(0, 0);
+	cv::waitKey(0);
+	cv::destroyWindow(windowName);
+
+	std::cout << "Cut start set to frame " << cutStartFrame << std::endl;
+
+	std::cout << "Drag the timeline just after the end of a squat. Press any key to confirm." << std::endl;
+
+	cv::namedWindow(windowName, 1);
+	cv::createTrackbar("Trim end", windowName, 0, initialFrameCount - 1, trimEndCallbeck);
+	trimEndCallbeck(0, 0);
+	cv::waitKey(0);
+	cv::destroyWindow(windowName);
+
+	std::cout << "Cut end set to frame " << cutEndFrame << std::endl;
+
+	// Debugging values. remove
+	/*cutStartFrame = 0;
+	cutEndFrame = initialFrameCount - 1;*/
+
+	cutLength = cutEndFrame - cutStartFrame;
+	std::cout << "Cut length " << cutLength << " frames" << std::endl;
+}
+
+static void timeLineDragCallback(int val, void* object) {
+	// Trackbar is zero-based
+	if(val < cutLength) {
+		currFrame = val + cutStartFrame;
+		cv::imshow(windowName, timeLine[currFrame]);
+	} else {
+		std::cout << "timeLine not ok" << std::endl;
+	}
+}
+
+static void trimStartCallbeck(int val, void* object) {
+	if(val < timeLine.size()) {
+		currFrame = val;
+		cutStartFrame = val;
+		cv::imshow(windowName, timeLine[currFrame]);
+	} else {
+		std::cout << "timeLine not ok" << std::endl;
+	}
+}
+
+static void trimEndCallbeck(int val, void* object) {
+	if(val < timeLine.size()) {
+		currFrame = val;
+		cutEndFrame = val;
+		cv::imshow(windowName, timeLine[currFrame]);
+	} else {
+		std::cout << "timeLine not ok" << std::endl;
+	}
+}
 
 static bool askYesNo(const char* promt) {
 	std::cout << promt << " (y/n)" << std::endl;
@@ -230,131 +274,4 @@ static int askInt(const char* promt) {
 		std::cout << "Value (" << ansS << ") out of integer range (" << INT_MIN << "," << INT_MAX << ")." << std::endl;
 		return askInt(promt);
 	}
-}
-
-/*
-Load a video to the timeLine and ask for video cut.
-*/
-static void loadVideo(std::string &vidPath) {
-
-	// TODO: siirrä mainiin tää sotku, jos vaikka binarisointi toimis
-
-	cv::VideoCapture cap(vidPath);
-	cv::Mat frame;
-
-	if(!cap.isOpened()) {
-		std::cerr << "Opening video file from " << vidPath << " failed." << std::endl;
-	}
-
-	std::cout << "Loading video... " << std::endl;
-
-	// take the first frame for getting some basic info about the video
-	cap.read(frame);
-
-	timePerFrameMs = cap.get(cv::CAP_PROP_POS_MSEC);
-	frameWidth = cap.get(cv::CAP_PROP_FRAME_WIDTH);
-	frameHeight = cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-	initialFrameCount = cap.get(cv::CAP_PROP_FRAME_COUNT);
-	cutEndFrame = initialFrameCount;
-	roi.width = frameWidth;
-	roi.height = frameHeight;
-
-	cv::namedWindow(windowCutStart, 1);
-	//cv::namedWindow(windowCutEnd, 1);
-
-	timeLine = std::vector<cv::Mat>(initialFrameCount);
-
-	timeLine[0] = frame.clone();
-	FrameUtils::rotate_90n(timeLine[0], timeLine[0], rotation);
-	cv::imshow(windowCutStart, timeLine[0]);
-	//cv::waitKey(0);
-	for(size_t i = 1; i < timeLine.size(); ++i) {
-		cap.read(frame);
-		timeLine[i] = frame.clone();
-		FrameUtils::rotate_90n(timeLine[i], timeLine[i], rotation);
-	}
-	cap.release();
-	std::cout << "done" << std::endl;
-
-	int startFrame = 0;
-	int lastFrame = initialFrameCount - 1;
-
-	/*
-	cv::createTrackbar("Frame", windowCutStart, &startFrame, lastFrame, cropStart);
-	cropStart(startFrame, 0);
-
-	cv::imshow(windowName, timeLine[0]);
-	while(cv::waitKey(1) < 0);
-
-	startFrame = cutStartFrame;
-	std::cout << "Cut start set to frame " << cutStartFrame << std::endl;
-
-	cv::createTrackbar("Frame", windowCutEnd, &startFrame, lastFrame, cropEnd);
-	cropEnd(startFrame, 0);
-
-	cv::imshow(windowName, timeLine[cutStartFrame]);
-	while(cv::waitKey(1) < 0);
-
-	std::cout << "Cut end set to frame " << cutEndFrame << std::endl;
-	*/
-
-	// Debugging values. remove
-	cutStartFrame = 0;
-	cutEndFrame = initialFrameCount - 1;
-	cutLength = cutEndFrame - cutStartFrame;
-
-	cv::destroyWindow(windowCutStart);
-	//cv::destroyWindow(windowCutEnd);
-}
-
-static void trimStartCallbeck(int val, void* object) {
-
-	if(val < timeLine.size() && val >= 0) {
-		currFrame = val;
-		cutStartFrame = val;
-		cv::imshow(windowCutStart, timeLine[currFrame]);
-	} else {
-		std::cout << "timeLine not ok" << std::endl;
-	}
-}
-static void trimEndCallbeck(int val, void* object) {
-
-	if(val < timeLine.size() && val >= 0) {
-		currFrame = val;
-		cutStartFrame = val;
-		cv::imshow(windowCutStart, timeLine[currFrame]);
-	} else {
-		std::cout << "timeLine not ok" << std::endl;
-	}
-}
-
-
-static void detectLines(cv::Mat &src_gray, cv::Mat &src_display) {
-
-	cv::Mat display = src_display.clone();
-	cv::Mat src = src_gray.clone();
-	//cv::threshold(src, src, 155, 255, 1);
-	Canny(src, src, 50, 200, 3);
-
-	std::vector<cv::Vec4i> lines;
-
-	HoughLinesP(src, lines, 1, CV_PI / 180, 50, 50, 10);
-
-	if(lines.size() == 0) {
-		std::cout << "No lines detected." << std::endl;
-	} else {
-		std::cout << "Detected " << lines.size() << " lines. " << std::endl;
-		/// Draw the detected lines
-		for(size_t i = 0; i < lines.size(); i++) {
-			cv::Vec4i l = lines[i];
-			cv::Point p1(l[0], l[1]);
-			cv::Point p2(l[2], l[3]);
-			if(abs(p1.x - p2.x) < 15) {
-				std::cout << "Detected a vertical line. " << std::endl;
-				cv::line(display, p1, p2, cv::Scalar(0, 0, 255), 3, cv::LINE_AA);
-			}
-		}
-	}
-	cv::namedWindow("Detected lines", cv::WINDOW_AUTOSIZE);
-	cv::imshow("Detected lines", display);
 }
