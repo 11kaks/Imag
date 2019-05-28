993ce09c993e999f4ecca1b43b240d458b700454
Kimmo Riihiaho <kimmo.riihiaho@gmail.com>
2019-05-28 08:37:55 +0300

    Fixed work formula

--- Imag/Squatter.h
+++ Imag/Squatter.h
@@ -4,6 +4,11 @@
 #include <fstream>
 #include <vector>
 
+/*
+Squat analysis class. 
+
+Computes velocities, forces etc. from a given pointlist.
+*/
 class Squatter
 {
 
@@ -17,10 +22,10 @@ public:
 	// Elapsed time between points. In seconds.
 	float timeStep;
 
-	// Initially position in pixels from the top of the window.
-	std::vector<int> listPos;
-	// Distance moved in meters during one frame.
-	std::vector<float> listPosScaled;
+	// Position in pixels from the top of the window.
+	std::vector<int> pixelPosition;
+	// Position in meters relative to the position in the first frame (which pos is 0).
+	std::vector<float> listRelativeScaledPos;
 	// Velocity
 	std::vector<float> listVel;
 	// Acceleration
@@ -31,36 +36,36 @@ public:
 	std::vector<float> listWork;
 	// Power of the work.
 	std::vector<float> listPower;
-
-	// Index of frames where the direction changes.
-	std::vector<int> directionChanges;
-
+	
 	Squatter(std::vector<int> pos)
 	:
 		barbellMass(0.f),
 		personHeight(1.7f),
 		timeStep(0.033f),
-		listPos(pos)
+		pixelPosition(pos)
 	{
 		// Estimate of the length of the stroke in relation to the squatters height
 		squatDistance = personHeight * 0.46f;
-		listPosScaled = std::vector<float>(listPos.size(), 0.f);
-		listVel = std::vector<float>(listPos.size(), 0.f);
-		listAcc = std::vector<float>(listPos.size(), 0.f);
-		listForce = std::vector<float>(listPos.size(), 0.f);
-		listWork = std::vector<float>(listPos.size(), 0.f);
-		listPower = std::vector<float>(listPos.size(), 0.f);
-		listPosScaled[0] = 0.f;
+		listRelativeScaledPos = std::vector<float>(pixelPosition.size(), 0.f);
+		listVel = std::vector<float>(pixelPosition.size(), 0.f);
+		listAcc = std::vector<float>(pixelPosition.size(), 0.f);
+		listForce = std::vector<float>(pixelPosition.size(), 0.f);
+		listWork = std::vector<float>(pixelPosition.size(), 0.f);
+		listPower = std::vector<float>(pixelPosition.size(), 0.f);
+		listRelativeScaledPos[0] = 0.f;
 		listVel[0] = 0.f;
 		listAcc[0] = 0.f;
 	}
 	~Squatter() {}
 
+	/*
+	Calculate things for all frames. After this we are ready for printing.
+	*/
 	void analyze() {
 		int highestPoint = -1;
 		int lowestPoint = 50000;
-		for(size_t i = 0; i < listPos.size(); ++i) {
-			int val = listPos[i];
+		for(size_t i = 0; i < pixelPosition.size(); ++i) {
+			int val = pixelPosition[i];
 			if(val < lowestPoint) {
 				lowestPoint = val;
 			}
@@ -70,32 +75,28 @@ public:
 		}
 		// Estimate how long distance in real world is one pixel in the video.
 		float scale = -squatDistance / (highestPoint - lowestPoint);
-		for(size_t i = 1; i < listPos.size(); ++i) {
-			float deltaY = scale * (listPos[i] - listPos[i - 1]);
-			listPosScaled[i] = (listPos[i] - listPos[0]) * scale;
-			if(i == 1){
-				listVel[i] = deltaY / timeStep;
-			}
+		for(size_t i = 1; i < pixelPosition.size(); ++i) {
+			float deltaY = scale * (pixelPosition[i] - pixelPosition[i - 1]);
+			listRelativeScaledPos[i] = (pixelPosition[i] - pixelPosition[0]) * scale;
 			// Exponential smoothing for velocity
 			// https://en.wikipedia.org/wiki/Exponential_smoothing
 			float alpha = 0.40f;
-			listVel[i] = alpha*(deltaY / timeStep) + (1-alpha)*listVel[i-1];
-			float acc = (listVel[i] - listVel[i - 1]) / timeStep;
-			listAcc[i] = acc;
+			listVel[i] = alpha * (deltaY / timeStep) + (1 - alpha)*listVel[i - 1];			
+			listAcc[i] = (listVel[i] - listVel[i - 1]) / timeStep;
 			// Force clamped to zero because we want to know about the 
 			// upstroke of the squatter
 			float force = listAcc[i] * barbellMass;
-			if(force > 0.f) {
+			//if(force > 0.f) {
 				listForce[i] = force;
-			}
-			float work = listForce[i] * listVel[i] * timeStep;
-			if(work > 0.f) {
+			//}
+			float work = listForce[i] * deltaY;
+			//if(work > 0.f) {
 				listWork[i] = work;
-			}
+			//}
 			float power = (listWork[i] - listWork[i-1]) / timeStep;
-			if(power > 0.f) {
+			//if(power > 0.f) {
 				listPower[i] = power;
-			}
+			//}
 		}
 	}
 
@@ -106,8 +107,8 @@ public:
 		char sep = ',';
 		std::cout << "time [s]" << sep << "position [m]" << sep << "velocity [m/s]" << sep << "acceleration [m/s^2]" << sep << "force [N]" << sep << "work [J]" << sep << "power [W]" << std::endl;
 
-		for(size_t i = 0; i < listPos.size(); ++i) {
-			std::cout << timeStep * i << sep << listPosScaled[i] << sep << listVel[i] << sep << listAcc[i] << sep << listForce[i] << sep << listWork[i] << sep << listPower[i] << std::endl;
+		for(size_t i = 0; i < pixelPosition.size(); ++i) {
+			std::cout << timeStep * i << sep << listRelativeScaledPos[i] << sep << listVel[i] << sep << listAcc[i] << sep << listForce[i] << sep << listWork[i] << sep << listPower[i] << std::endl;
 		}
 	}
 };
